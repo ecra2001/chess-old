3b3711df79eaea85a1be234110c1b1c78cc9327b
ecra2001 <ecra6157@byu.edu>
2024-10-21 22:12:04 -0600

    implemented User Repositoty

--- server/src/main/java/dataaccess/MemoryRep.java
+++ server/src/main/java/dataaccess/MemoryRep.java
@@ -1,16 +1,51 @@
 package dataaccess;
 
+import model.UserData;
+import java.util.HashSet;
+
 public class MemoryRep {
 
-  public static class InMemoryAuthenticationRepository implements AuthRep {
+  public static class MemoryAuth implements AuthRep {
 
   }
 
-  public static class InMemoryGameRepository implements GameRep {
+  public static class MemoryGame implements GameRep {
 
   }
 
-  public static class InMemoryUserRepository implements UserRep {
+  public static class MemoryUser implements UserRep {
+    private HashSet<UserData> db;
 
+    MemoryUser() {
+      db = HashSet.newHashSet(16);
+    }
+
+    @Override
+    public UserData getUser(String username) {
+      for (UserData user : db) {
+        if (user.username().equals(username)) {
+          return user;
+        }
+      }
+      return null;
+    }
+    @Override
+    public void createUser(String username, String password, String email) {
+      db.add(new UserData(username, password, email));
+    }
+    @Override
+    public boolean authenticateUser(String username, String password) {
+      for (UserData user : db) {
+        if (user.username().equals(username) &&
+                user.password().equals(password)) {
+          return true;
+        }
+      }
+      return false;
+    }
+    @Override
+    public void clear() {
+      db = HashSet.newHashSet(16);
+    }
   }
 }
--- server/src/main/java/dataaccess/UserRep.java
+++ server/src/main/java/dataaccess/UserRep.java
@@ -1,4 +1,10 @@
 package dataaccess;
 
+import model.UserData;
+
 public interface UserRep {
+  UserData getUser(String username);
+  void createUser(String username, String password, String email);
+  boolean authenticateUser(String username, String password);
+  void clear();
 }
