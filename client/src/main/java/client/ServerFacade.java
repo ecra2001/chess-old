7646f2be20f8bffe8d2be204c718d4cd68bda21b
ecra2001 <ecra6157@byu.edu>
2024-11-11 22:04:16 -0700

    updated ServerFacade.java

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,4 +1,52 @@
 package client;
-
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
 public class ServerFacade {
+  String baseURL = "http://localhost:8080";
+  String authToken;
+  ServerFacade() {
+  }
+  public boolean register(String username, String password, String email) {
+    var body = Map.of("username", username, "password", password, "email", email);
+    var jsonBody = new Gson().toJson(body);
+    return post("/user", jsonBody);
+  }
+  public boolean login(String username, String password) {
+    var body = Map.of("username", username, "password", password);
+    var jsonBody = new Gson().toJson(body);
+    return post("/session", jsonBody);
+  }
+  public boolean post(String endpoint, String body) {
+    try {
+      URI uri = new URI(baseURL + endpoint);
+      HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
+      http.setRequestMethod("POST");
+      http.setDoOutput(true);
+      http.addRequestProperty("Content-Type", "application/json");
+      try (var outputStream = http.getOutputStream()) {
+        outputStream.write(body.getBytes());
+      }
+      http.connect();
+      try {
+        if (http.getResponseCode() == 401) {
+          return false;
+        }
+      } catch (IOException e) {
+        return false;
+      }
+      try (InputStream respBody = http.getInputStream()) {
+        InputStreamReader inputStreamReader = new InputStreamReader(respBody);
+        authToken = (String) new Gson().fromJson(inputStreamReader, Map.class).get("authToken");
+      }
+    } catch (URISyntaxException | IOException e) {
+      return false;
+    }
+    return true;
+  }
 }
