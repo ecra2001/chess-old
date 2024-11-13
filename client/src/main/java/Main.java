7cec013be4f6485581e47dae005dbf23b840a9c8
ecra2001 <ecra6157@byu.edu>
2024-11-12 22:08:53 -0700

    cleanup main.java

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -4,26 +4,10 @@ import ui.PreLogin;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.println("♕ 240 Chess Client:");
 
         ServerFacade server = new ServerFacade();
         PreLogin prelogin = new PreLogin(server);
         prelogin.run();
-//        System.out.println(server.register("username", "password", "email"));
-//
-//        System.out.println(server.login("username", "password"));
-//
-//        System.out.println(server.logout());
-//
-//        int gameID = server.createGame("game1");
-//
-//        System.out.println(gameID);
-//
-//        System.out.println(server.listGames());
-//
-//        System.out.println(server.joinGame(gameID, "WHITE"));
-//
-//        System.out.println(server.listGames());
     }
 }
