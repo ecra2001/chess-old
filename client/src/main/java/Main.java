9e392b368b91bfe28c4fb19ea22a5db132877efb
ecra2001 <ecra6157@byu.edu>
2024-12-02 23:25:40 -0700

    added loadgame and notification

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,14 +1,14 @@
-import chess.*;
-import client.ServerFacade;
-import ui.PreLogin;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("♕ 240 Chess Client:");
-
-        ServerFacade server = new ServerFacade();
-        PreLogin prelogin = new PreLogin(server);
-        prelogin.run();
-        System.out.println("Exited");
-    }
-}
+//import chess.*;
+//import client.ServerFacade;
+//import ui.PreLogin;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("♕ 240 Chess Client:");
+//
+//        ServerFacade server = new ServerFacade();
+//        PreLogin prelogin = new PreLogin(server);
+//        prelogin.run();
+//        System.out.println("Exited");
+//    }
+//}
